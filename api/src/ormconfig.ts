3bf3c43a9b716f9bf1e67120624087fce1eb2637
Oleh Bratok <151098313+Takemyeye@users.noreply.github.com>
2025-02-23 00:17:25 +0100

    /

--- api/src/ormconfig.ts
+++ api/src/ormconfig.ts
@@ -6,11 +6,11 @@ import { User } from './entities/user';
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3306,
-  username: process.env.DB_USER || 'dev',
-  password: process.env.DB_PASSWORD || '12345678987654321',
-  database: process.env.DB_NAME || 'VoiceDream',
+  host: process.env.DB_HOST || '',
+  port: Number(process.env.DB_PORT) || 0,
+  username: process.env.DB_USER || '',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || '',
   synchronize: false,
   logging: true,
   entities: [User, Stories, Voice, UserStory],
